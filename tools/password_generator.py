eb9bf96b6d485ae1d538228930a6b157cc151f1a
harsh <harshtpo13@gmail.com>
2026-01-15 19:46:52 +0530

    added a loop for adding characters instead of repeating code blocks

--- tools/password_generator.py
+++ tools/password_generator.py
@@ -3,11 +3,16 @@ import string
 
 def generate_password():
     l = string.ascii_lowercase
-    u= string.ascii_uppercase
+    u = string.ascii_uppercase
     d = string.digits
     n = "!@#$%^&*()_-+="
+    random_chars = []
 
-    random_chars =  [sc.choice(l) for i in range(3+sc.randbelow(3))] + [sc.choice(u) for i in range(3+sc.randbelow(3))] + [sc.choice(n) for i in range(2+sc.randbelow(3))] + [sc.choice(d) for i in range(2+sc.randbelow(3))]
+    for chrs in [l, u, d, n]:
+        for i in range(3 + sc.randbelow(3)):
+            ch = sc.choice(chrs)
+            random_chars.append(ch)
+    
     sc.SystemRandom().shuffle(random_chars)
     password = ''.join(random_chars)
     return password
